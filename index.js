17ffb2236dfa5861b14703c84bcc53b356613b5f
Tom Palmer <tom@localhost.localdomain>
2020-03-08 15:00:41 +0000

    Starting to take shape

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+// Grab requirements
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require("request");
 
-// Creates express app
+// Initialise the listener
 const app = express();
-// The port used for Express server
 const PORT = 3000;
+
 // Starts server
 app.listen(process.env.PORT || PORT, function() {
   console.log('Bot is listening on port ' + PORT);
@@ -14,14 +15,42 @@ app.listen(process.env.PORT || PORT, function() {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.post('/', (req, res) => {
-var data = {form: {
-      token: process.env.SLACK_AUTH_TOKEN,
-      channel: "#general",
-      text: "Hi! :wave: \n I'm your new bot."
-    }};
-request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
-      // Sends welcome message
-      res.json();
-    });
+
+/* --------------------- Test that everything is working -------------------- */
+app.post('/test', (req, res) => {
+  var data = {form: {
+        token: process.env.SLACK_AUTH_TOKEN,
+        channel: "#general",
+        text: "Hi! :wave: \n I'm your new bot."
+      }};
+      console.log(data);
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+        // Sends welcome message
+        var msg = res.json();
+        if (msg.statusCode != 200) {
+          console.log("An error has ocurred: ", error);
+        }else{
+          console.log(body);
+        }
+        
+      });
+});
+
+/* ----------------------- Legit ping command is legit ---------------------- */
+app.post('/ping', (req, res) => {
+  var data = {form: {
+        token: process.env.SLACK_AUTH_TOKEN,
+        channel: "#general",
+        text: "pong"
+      }};
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+        // Sends welcome message
+        var msg = res.json();
+        if (msg.statusCode != 200) {
+          console.log("An error has ocurred: ", error);
+        }else{
+          console.log(JSON.parse(body));
+        }
+
+      });
 });
