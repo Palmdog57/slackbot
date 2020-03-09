3631bcf91f8ca4eed050571950630cd84eb76050
Tom Palmer <tompalmer12@gmail.com>
2020-03-09 18:29:02 +0000

    Added a flag to ensure repsonse headers don't crash the script

--- gifController.js
+++ gifController.js
@@ -3,27 +3,40 @@
 const request = require('request');
 const morning_gif = require('./data.json');
 const chalk = require('chalk');
-const debug = true;
+const debug = false;
 
 /** Return a random cat GIF to the channel the message originated from
  *  @todo Cleanup 200 OK request on line 12 
  *  @require request
  */
-function lolcats(app){
+function lolcats(app, cb){
     app.post('/cat', (req, res) => {
         res.end(); //Send a 200 okay message to slack to avoid timeout error being displayed to the user
         console.log("\nCOMMAND: /cat");
 
         // Query the cat API and set the body of the response as our slack message
         request('http://edgecats.net/random', function (error, response, body) {
-            if (response.statusCode != 200) console.error(error);
+            if( response.headers['content-length'] > 200){
+                console.error(chalk.red("413 - Payload Too Large"));
+                console.error(chalk.red("Going for shutdown"));
+                process.exit();
+            }
+            if (response.statusCode != 200) {
+                console.error("CAT RECEIPT:", chalk.red(response.statusCode));
+                console.error(error);
+
+                var msgToSend = "Error occurred while trying to find the cats :crying_cat_face:"
+            }else{
+                console.log("CAT RECEIPT:", chalk.green(response.statusCode));
+                var msgToSend = body
+            }
 
             // Construct the data for our slack response
             const data = {
                 form: {
                     token: process.env.SLACK_AUTH_TOKEN,
                     channel: req.body.channel_name,
-                    text: body
+                    text: msgToSend
                 }
             };
 
@@ -36,7 +49,7 @@ function lolcats(app){
 /** Returns a message to the channel && a good morning GIF
  *  Picks the GIF from a JSON object specifed in ./data.json
  *  Displays a message sent by the user. If none exist, a JSON array of greetings is called
- *  If all else fails, the message displayed is 'Good morning'
+ *  If no greeting is found, the message displayed is 'Good morning'
  *  @require ./data.json
  */
 function morning(app){
